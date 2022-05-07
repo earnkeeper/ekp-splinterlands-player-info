0ae4e3a6f98be3245d5d109087cbed7c724eb13d
gavin-shaw <gavin@gnarus.io>
2022-05-07 14:35:27 +0200

    chore: upgrade to latest SDK

--- src/worker.module.ts
+++ src/worker.module.ts
@@ -1,4 +1,5 @@
-import { EkConfigService, SdkModule } from '@earnkeeper/ekp-sdk-nestjs';
+import { config } from '@earnkeeper/ekp-sdk';
+import { SdkModule } from '@earnkeeper/ekp-sdk-nestjs';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CollectionModule } from './feature/collection/collection.module';
@@ -6,7 +7,11 @@ import { HistoryModule } from './feature/history/history.module';
 
 export const MODULE_DEF = {
   imports: [
-    MongooseModule.forRootAsync({ useClass: EkConfigService }),
+    MongooseModule.forRootAsync(
+      config('MONGO_URI', {
+        default: 'mongodb://localhost:27017/splinterlands',
+      }),
+    ),
     CollectionModule,
     HistoryModule,
     SdkModule,
